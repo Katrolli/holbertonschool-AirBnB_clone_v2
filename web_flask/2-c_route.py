f454ee1b6177d3c92aa79571b68e36340009b769
Katrolli <ennio_cano@live.com>
2023-04-03 13:24:13 +0200

    third route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""First flask app"""
+from flask import Flask
+
+app = Flask(__name__)
+""" Flask app module"""
+
+
+@app.route("/", strict_slashes=False)
+def first_route():
+    """ Route that prints hello """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def second_route():
+    """ Route that prints hello """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def third_route(text):
+    """ Route that prints hello """
+    return "C {}".format(text.replace('_', " "))
+
+
+if __name__ == '__main__':
+    """ sa per siguri """
+    app.run(host='0.0.0.0', port=5000, debug=True)
