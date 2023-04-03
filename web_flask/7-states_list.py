267920c56ffa7ee1f961f38e8aafe78d68ac2117
Katrolli <ennio_cano@live.com>
2023-04-03 15:03:17 +0200

    testing flask with engine

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""First flask app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+""" Flask app module"""
+
+
+@app.teardown_appcontext
+def closure():
+    return storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def state_list():
+    """ Route that prints hello """
+    states = storage.all(State)
+    return render_template('states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    """ sa per siguri """
+    app.run(host='0.0.0.0', port=5000, debug=True)
