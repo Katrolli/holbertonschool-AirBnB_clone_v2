9481ddb8655db54451f3de58d9a260c826777eab
Katrolli <ennio_cano@live.com>
2023-04-04 13:10:38 +0200

    testing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -16,7 +16,7 @@ def state_city_list():
     """ Route that prints hello """
     states = storage.all("State").values()
     for state in states:
-        cities = type(state)
+        cities = state.cities
     return render_template('8-cities_by_states.html', states=states, city=cities)
 
 
