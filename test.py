027a6a1724f6fa686192283af5d24e47bea66ba0
Katrolli <ennio_cano@live.com>
2023-04-05 12:44:49 +0200

    html editing

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from models import storage
 from models.state import State
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
+from models.city import City
+from models.amenity import Amenity
 
-cities_by_state = {}
-states = storage.all("State").values()
-for state in states:
-    print(state.cities)
+
+db = storage.all().values()
